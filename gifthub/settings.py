5782b60ab93d992e54ef6f88efdb77128982c02c
EddyJavs <eddjavs@gmail.com>
2020-10-16 18:13:00 -0600

    se agrego templates

--- gifthub/settings.py
+++ gifthub/settings.py
@@ -39,10 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     # 3tr party
+    'crispy_forms',
+    'allauth',
+    'allauth.account',
     # Local apps
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
-
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
 # Custom user model
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -146,3 +151,22 @@ EMAIL_HOST=os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT=os.environ.get('EMAIL_PORT')
+
+# Django allauth config
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT='home'
+
+AUTHENTICATION_BACKENDS=(
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
+
+ACCOUNT_SESSION_REMEMBER =True
+ACCOUNT_LOGOUT_ON_GET=True
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD='email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
