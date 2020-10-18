f60d377b85bfab02fdfadae13c52c9ed914d8651
Eddy <eddjavs@gmail.com>
2020-10-17 20:24:13 -0600

    Se realizo funcion convertir precio y se realizo test con mock

--- CardCatalog/views.py
+++ CardCatalog/views.py
@@ -5,3 +5,8 @@ import requests
 def getAPI_TasaCambio():
     response = requests.get('https://my-json-server.typicode.com/CoffeePaw/AyD1API/TasaCambio')
     return response.json()
+
+def convertirPrecio(valor):
+    tipoCambio=getAPI_TasaCambio()[0].get("total")
+    valorConvertido=float(tipoCambio)*valor
+    return valorConvertido
